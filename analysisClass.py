3aab06aa51605d7797ff5d3b72da88ec401057ac
leogior <98914690+leogior@users.noreply.github.com>
2025-01-26 14:28:12 +0100

    Add files via upload

--- analysisClass.py
+++ analysisClass.py
@@ -6,13 +6,33 @@ import plotly.graph_objects as go
 from orderBookClass import OBData
 from datetime import datetime
 from debug import logger
+from SQLite_Manager.sqlManager import SqlAlchemyDataBaseManager
 
 
 class analysisClass:
-    def __init__(self, autoTrader):
+    def __init__(self, autoTrader, path : str = None, dashboardName : str = None, dbName : str = None):
         self.autoTrader = autoTrader
+        self.path = path
+        self.dashboardName = dashboardName
+        self.dbName = dbName
 
-    def dashboard(self):
+        self.time = OBData.OBData_[:,OBData.OBIndex["eventTime"]]
+        self.time  = pd.to_datetime(self.time, unit='ms')
+        self.bids = OBData.OBData_[:,OBData.OBIndex["bids"]]
+        self.asks = OBData.OBData_[:,OBData.OBIndex["asks"]]
+        self.mid = (self.bids+self.asks)/2
+
+        self.historicalTrades = pd.DataFrame(self.autoTrader.historical_trade, columns=["idx", "sendTime", "price", "quantity", "endTime", "status"])
+        self.historicalTrades["sendTime"] = pd.to_datetime(self.historicalTrades["sendTime"], unit='ms')
+        self.historicalTrades["endTime"] = pd.to_datetime(self.historicalTrades["endTime"], unit='ms')
+
+        self.historicalInventory = pd.DataFrame({"time":self.time,"inventory":self.autoTrader.historical_inventory})
+        self.historicalPnL = pd.DataFrame({"time":self.time,"Pnl":self.autoTrader.historical_pnl})
+        self.historicalUnrealizedPnL = pd.DataFrame({"time":self.time,"unrealPnl":self.autoTrader.historical_unrealPnL})
+
+        self.df_mid = pd.DataFrame({"time":self.time, "mids":self.mid})
+
+    def dashboard(self, save = True, show = False):
 
         fig = make_subplots(specs=[[{"secondary_y": True}], [{}]], 
                             rows=2, cols=1,
@@ -22,47 +42,33 @@ class analysisClass:
         fig.update_layout(title_text="Execution Dashboard")
         
 
-        time = OBData.OBData_[:,OBData.OBIndex["eventTime"]]
-        time  = pd.to_datetime(time, unit='ms')
-        bids = OBData.OBData_[:,OBData.OBIndex["bids"]]
-        asks = OBData.OBData_[:,OBData.OBIndex["asks"]]
-        mid = (bids+asks)/2
 
-        historicalTrades = pd.DataFrame(self.autoTrader.historical_trade, columns=["idx", "sendTime", "price", "quantity", "endTime", "status"])
-        historicalTrades["sendTime"] = pd.to_datetime(historicalTrades["sendTime"], unit='ms')
-        historicalTrades["endTime"] = pd.to_datetime(historicalTrades["endTime"], unit='ms')
-
-        historicalInventory = pd.DataFrame({"time":time,"inventory":self.autoTrader.historical_inventory})
-        historicalPnL = pd.DataFrame({"time":time,"Pnl":self.autoTrader.historical_pnl})
-        historicalUnrealizedPnL = pd.DataFrame({"time":time,"unrealPnl":self.autoTrader.historical_unrealPnL})
-
-        df_mid = pd.DataFrame({"time":time, "mids":mid})
         
         fig.add_trace(go.Scatter(
-                            x=df_mid.time,
-                            y=df_mid.mids,
+                            x=self.df_mid.time,
+                            y=self.df_mid.mids,
                             name="mid price"
                                 )
                         ,row=1, col=1, secondary_y=False)
     
         fig.add_trace(go.Scatter(
-                        x=historicalPnL.time,
-                        y=historicalPnL.Pnl,
+                        x=self.historicalPnL.time,
+                        y=self.historicalPnL.Pnl,
                         name="pnl"
                             )
                     ,row=1, col=1, secondary_y=True)
 
         fig.add_trace(go.Scatter(
-                        x=historicalUnrealizedPnL.time,
-                        y=historicalUnrealizedPnL.unrealPnl,
+                        x=self.historicalUnrealizedPnL.time,
+                        y=self.historicalUnrealizedPnL.unrealPnl,
                         name="UnrealPnl"
                             )
                     ,row=1, col=1, secondary_y=True)
         
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[historicalTrades.quantity > 0].sendTime,  # Timestamps for buy signals
-                y=historicalTrades[historicalTrades.quantity > 0].price,  # Prices at which buy signals occurred
+                x=self.historicalTrades[self.historicalTrades.quantity > 0].sendTime,  # Timestamps for buy signals
+                y=self.historicalTrades[self.historicalTrades.quantity > 0].price,  # Prices at which buy signals occurred
                 mode='markers',
                 marker=dict(color='green', size=10, symbol='triangle-up'),
                 name="Buy Sent"
@@ -73,8 +79,8 @@ class analysisClass:
 
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[(historicalTrades.quantity > 0) & (historicalTrades.status==1)].endTime,  # Timestamps for buy signals
-                y=historicalTrades[(historicalTrades.quantity > 0) & (historicalTrades.status==1)].price,  # Prices at which buy signals occurred
+                x=self.historicalTrades[(self.historicalTrades.quantity > 0) & (self.historicalTrades.status==1)].endTime,  # Timestamps for buy signals
+                y=self.historicalTrades[(self.historicalTrades.quantity > 0) & (self.historicalTrades.status==1)].price,  # Prices at which buy signals occurred
                 mode='markers',
                 marker=dict(color='green', size=10, symbol='triangle-up'),
                 name="Buy Filled"
@@ -85,8 +91,8 @@ class analysisClass:
 
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[(historicalTrades.quantity > 0) & (historicalTrades.status==-1)].endTime,  # Timestamps for buy signals
-                y=historicalTrades[(historicalTrades.quantity > 0) & (historicalTrades.status==-1)].price,  # Prices at which sell signals occurred
+                x=self.historicalTrades[(self.historicalTrades.quantity > 0) & (self.historicalTrades.status==-1)].endTime,  # Timestamps for buy signals
+                y=self.historicalTrades[(self.historicalTrades.quantity > 0) & (self.historicalTrades.status==-1)].price,  # Prices at which sell signals occurred
                 mode='markers',
                 marker=dict(color='blue', size=10, symbol='triangle-up'),
                 name="Buy Cancelled"
@@ -97,8 +103,8 @@ class analysisClass:
 
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[(historicalTrades.quantity < 0)].sendTime,  # Timestamps for buy signals
-                y=historicalTrades[(historicalTrades.quantity < 0)].price,  # Prices at which sell signals occurred
+                x=self.historicalTrades[(self.historicalTrades.quantity < 0)].sendTime,  # Timestamps for buy signals
+                y=self.historicalTrades[(self.historicalTrades.quantity < 0)].price,  # Prices at which sell signals occurred
                 mode='markers',
                 marker=dict(color='red', size=10, symbol='triangle-down'),
                 name="Sell Sent"
@@ -109,8 +115,8 @@ class analysisClass:
 
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[(historicalTrades.quantity < 0) & (historicalTrades.status==1)].endTime,  # Timestamps for buy signals
-                y=historicalTrades[(historicalTrades.quantity < 0) & (historicalTrades.status==1)].price,  # Prices at which sell signals occurred
+                x=self.historicalTrades[(self.historicalTrades.quantity < 0) & (self.historicalTrades.status==1)].endTime,  # Timestamps for buy signals
+                y=self.historicalTrades[(self.historicalTrades.quantity < 0) & (self.historicalTrades.status==1)].price,  # Prices at which sell signals occurred
                 mode='markers',
                 marker=dict(color='red', size=10, symbol='triangle-down'),
                 name="Sell Filled"
@@ -121,8 +127,8 @@ class analysisClass:
 
         fig.add_trace(
             go.Scatter(
-                x=historicalTrades[(historicalTrades.quantity < 0) & (historicalTrades.status==-1)].endTime,  # Timestamps for buy signals
-                y=historicalTrades[(historicalTrades.quantity < 0) & (historicalTrades.status==-1)].price,  # Prices at which sell signals occurred
+                x=self.historicalTrades[(self.historicalTrades.quantity < 0) & (self.historicalTrades.status==-1)].endTime,  # Timestamps for buy signals
+                y=self.historicalTrades[(self.historicalTrades.quantity < 0) & (self.historicalTrades.status==-1)].price,  # Prices at which sell signals occurred
                 mode='markers',
                 marker=dict(color='blue', size=10, symbol='triangle-down'),
                 name="Sell Cancelled"
@@ -132,10 +138,28 @@ class analysisClass:
         )
 
         fig.add_trace(go.Scatter(
-                            x=historicalInventory.time,
-                            y=historicalInventory.inventory,
+                            x=self.historicalInventory.time,
+                            y=self.historicalInventory.inventory,
                             name="inventory"
                                 )
                         ,row=2, col=1)
 
-        fig.write_html("test.html")
+        if show:
+            fig.show()
+        if save:
+            if self.path == None:
+                fig.write_html(f"{self.dashboardName}.html")
+            else:
+                fig.write_html(f"{self.path}/{self.dashboardName}.html")
+
+    def dataBase(self):
+
+        if self.path == None:
+            db = SqlAlchemyDataBaseManager(f"{self.dashboardName}.db")
+        else:
+            db = SqlAlchemyDataBaseManager(f"{self.path}/{self.dbName}.db")
+
+        db.update("historicalTrades",self.historicalTrades)
+        db.update("historicalInventory",self.historicalInventory)
+        db.update("historicalPnL",self.historicalPnL)
+        db.update("historicalUnrealizedPnL",self.historicalUnrealizedPnL)
